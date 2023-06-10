2a98ccfe95f792447f926e70060101b82124642a
Anderson-Reges <andersonreges76@gmail.com>
2023-06-10 16:33:00 -0300

    style: adjust features section

--- src/components/Features/FeaturesLeft.tsx
+++ src/components/Features/FeaturesLeft.tsx
@@ -4,7 +4,11 @@ import IFeature from '../../interfaces/IFeature';
 const FeaturesLeft: React.FC<IFeature> = ({ image, title, text }) => {
   return (
     <div className='flex gap-x-[2em] items-center'>
-      <div className='w-36 text-right'>
+      <div
+        className='w-[119px] h-[119px] desktop:hidden'
+        style={{ backgroundImage: `url(${image})` }}
+      ></div>
+      <div className='w-36 desktop:text-right mobile:text-left'>
         <h4
           className='text-xl text-background-3 font-semibold w-[145px]'
         >
@@ -13,7 +17,7 @@ const FeaturesLeft: React.FC<IFeature> = ({ image, title, text }) => {
         <p>{text}</p>
       </div>
       <div
-        className='w-[119px] h-[119px]'
+        className='w-[119px] h-[119px] desktop:block mobile:hidden'
         style={{ backgroundImage: `url(${image})` }}
       ></div>
     </div>
