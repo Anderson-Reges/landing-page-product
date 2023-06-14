4e9f1f36ed0755166459b8e9981b35035adfe0ff
Anderson-Reges <andersonreges76@gmail.com>
2023-06-14 12:33:39 -0300

    Feat: creating router Home

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import SlideProvider from './context/SlideProvider';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import SlideProvider from "./context/SlideProvider";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
+  <BrowserRouter>
     <SlideProvider>
       <App />
     </SlideProvider>
+  </BrowserRouter>
 );
