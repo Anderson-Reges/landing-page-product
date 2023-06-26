2bde23a72fcfd1c753eb5d547cba4860c2ca5290
Anderson-Reges <andersonreges76@gmail.com>
2023-06-26 17:27:39 -0300

    Feat: creating the incomplete cart section

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,9 +1,30 @@
 import React from "react";
 import Nav from "../components/Nav/Nav";
 import Footer from "../components/Footer";
-import angle from '../assets/angle-right-solid.svg'
+import angle from '../assets/angle-right-solid.svg';
+import IItemCart from "../interfaces/IItemCart";
+import headset from '../assets/headset.png';
+import { ItemContext } from "../context/ItemProvider";
 
 const Cart: React.FC = () => {
+  const { itemCart, setItemCart } = React.useContext(ItemContext);
+  const [quantity, setQuantity] = React.useState("")
+  const itemInStorage: IItemCart = JSON.parse(localStorage.getItem('cart') as string)
+
+  const updateCart = () => {
+    localStorage
+      .setItem("cart", JSON.stringify({
+        ...itemCart ,
+        quantity: quantity,
+        subtotal: itemCart.price * Number(quantity)
+      }))
+    setQuantity(quantity)
+  }
+
+  React.useEffect(() => {
+    const updatedItem: IItemCart = JSON.parse(localStorage.getItem('cart') as string)
+    setQuantity(updatedItem.quantity + "")
+  }, [])
 
   return (
     <>
@@ -28,6 +49,65 @@ const Cart: React.FC = () => {
           >Cart</span>
         </div>
       </header>
+      <section>
+        <div
+          className="py-[6em]"
+        >
+          <div className="flex justify-center">
+            <form
+              action=""
+              className="
+                flex flex-col justify-center items-center
+                w-[85%] h-[17em] px-[3em]
+                shadow-2xl"
+            >
+              <table className="w-full">
+                <thead className="text-left">
+                  <th className="w-[16%]"></th>
+                  <th className="w-[16%]"></th>
+                  <th className="w-[16%]">Product</th>
+                  <th className="w-[16%]">Price</th>
+                  <th className="w-[16%]">Quantity</th>
+                  <th className="w-[16%]">Subtotal</th>
+                </thead>
+                <tbody>
+                  <tr className="text-left">
+                    <td className="align-middle text-center">X</td>
+                    <td className="align-middle">
+                      <img src={headset} alt=""/>
+                    </td>
+                    <td className="align-middle">
+                      {itemInStorage.name}
+                    </td>
+                    <td className="align-middle">
+                      ${itemInStorage.price}
+                    </td>
+                    <td className="align-middle">
+                      <input
+                        type="number"
+                        value={quantity}
+                        onChange={ ({target}) => setQuantity(target.value) }
+                        className="rounded-full w-[5em]"
+                      />
+                    </td>
+                    <td className="align-middle">
+                      ${itemInStorage.subtotal}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <button
+                className="bg-primary w-44 h-11 justify-center
+                items-center rounded-3xl font-normal text-background-1"
+                onClick={ updateCart }
+              >
+                UPDATE CART
+              </button>
+            </form>
+          </div>
+          <div></div>
+        </div>
+      </section>
       <footer>
         <Footer />
       </footer>
