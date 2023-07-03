96214c7027b8e37babec48b6efc476d545ff2137
Anderson-Reges <andersonreges76@gmail.com>
2023-07-03 15:32:28 -0300

    Styles: adding responsive styling to features and specifInfos

--- src/components/SpecificInfo.tsx
+++ src/components/SpecificInfo.tsx
@@ -3,7 +3,7 @@ import woman from '../assets/woman.jpeg'
 
 const SpecificInfo: React.FC = () => {
   return (
-    <div className='relative desktop:h-[678px] mobile:h-[50em]'>
+    <div className='relative desktop:h-[678px] mobile:h-[50em] tablet:h-[30em]'>
       <div
         className='mobile:bg-left desktop:bg-center bg-cover w-full h-full'
         style={{ backgroundImage: `url(${woman})` }}
@@ -17,19 +17,19 @@ const SpecificInfo: React.FC = () => {
           <h1
             className='
             desktop:text-5xl mobile:text-2xl text-background-3
-            font-extrabold'
+            font-extrabold tablet:text-4xl tablet:w-[20em]'
           >
             DISCOVER THE TECHNOLOGY AND EXPERIENCE THAT GOES INTO OUR PRODUCTS
           </h1>
           <div
             className='
             flex desktop:flex-row desktop:gap-[9em]
-            mobile:gap-[2em] mobile:flex-col'
+            mobile:gap-[2em] mobile:flex-col tablet:flex-row'
           >
             <div
               className='
               desktop:bg-transparent mobile:bg-background-1 mobile:rounded-2xl
-              mobile:px-[1.5em] mobile:py-[1em]'
+              mobile:px-[1.5em] mobile:py-[1em] tablet:opacity-[0.9]'
             >
               <h2 className='flex desktop:text-8xl mobile:text-7xl mobile:items-baseline'>
                 40<p className='text-primary desktop:text-8xl mobile:text-6xl'>h</p>
@@ -40,7 +40,7 @@ const SpecificInfo: React.FC = () => {
             <div
               className='
               desktop:bg-transparent mobile:bg-background-1 mobile:rounded-2xl mobile:px-[1.5em]
-              mobile:py-[1em]'
+              mobile:py-[1em] tablet:opacity-[0.9]'
             >
               <h2 className='flex desktop:text-8xl mobile:text-7xl mobile:items-baseline'>
                 30<p className='text-primary mobile:text-6xl desktop:text-8xl'>min</p>
